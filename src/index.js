2c29dce69807f67418bd94956c130354da53096e
Ricardo <ricardo.a.nava@gmail.com>
2020-08-04 22:19:58 -0700

    Add selector functionality for cleaner code and future-proof scalability

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import store from "./store";
+import { STORY_ARCHIVE } from "./constants/actionTypes";
 import "./index.css";
+import store from "./store";
+import { getReadableStories } from "./selectors/story";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App stories={store.getState().storyState} onArchive={() => {}} />
+    <App
+      stories={getReadableStories(store.getState())}
+      onArchive={(id) => store.dispatch({ type: STORY_ARCHIVE, id })}
+    />
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/selectors/story.js
@@ -0,0 +1,7 @@
+const isNotArchived = (archivedIds) => (story) =>
+  archivedIds.indexOf(story.objectID) === -1;
+
+const getReadableStories = ({ storyState, archiveState }) =>
+  storyState.filter(isNotArchived(archiveState));
+
+export { getReadableStories };
