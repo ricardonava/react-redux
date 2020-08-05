7b3d37933c5110f1896597f4d533976a7dfd0144
Ricardo <ricardo.a.nava@gmail.com>
2020-08-04 22:51:11 -0700

    Refactor app to Lift connection To add more atomicity in props and state

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import { doArchiveStory } from "../actions/archive";
 import { getReadableStories } from "../selectors/story";
 import "./Stories.css";
 import Story from "./Story";
@@ -37,16 +36,11 @@ const StoriesHeader = ({ columns }) => (
   </div>
 );
 
-const Stories = ({ stories, onArchive }) => (
+const Stories = ({ stories }) => (
   <div className="stories">
     <StoriesHeader columns={COLUMNS} />
     {(stories || []).map((story) => (
-      <Story
-        key={story.objectID}
-        story={story}
-        columns={COLUMNS}
-        onArchive={onArchive}
-      />
+      <Story key={story.objectID} story={story} columns={COLUMNS} />
     ))}
   </div>
 );
@@ -55,8 +49,4 @@ const mapStateToProps = (state) => ({
   stories: getReadableStories(state),
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  onArchive: (id) => dispatch(doArchiveStory(id)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Stories);
+export default connect(mapStateToProps)(Stories);
