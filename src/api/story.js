e67e2e8bd50f4fcad400a62d0e5df5e3faf96353
Ricardo <ricardo.a.nava@gmail.com>
2020-08-04 23:13:33 -0700

    Refactor app to add separation of concerns Split the Fetch API from the Saga itself to be reused by other sagas

---
+++ src/api/story.js
@@ -0,0 +1,6 @@
+const HN_BASE_URL = "http://hn.algolia.com/api/v1/search?query=";
+
+const fetchStories = (query) =>
+  fetch(HN_BASE_URL + query).then((response) => response.json());
+
+export { fetchStories };
