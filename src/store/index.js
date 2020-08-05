8e9a333cf2a5697ac41d6034f0104b2f08c05f7b
Ricardo <ricardo.a.nava@gmail.com>
2020-08-04 23:04:09 -0700

    Add redux saga and minor boilerplate to Fetch

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,18 @@
 import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 import { createLogger } from "redux-logger";
 import rootReducer from "../reducers";
+import rootSaga from "../sagas";
 
 const logger = createLogger();
+const saga = createSagaMiddleware();
 
-const store = createStore(rootReducer, undefined, applyMiddleware(logger));
+const store = createStore(
+  rootReducer,
+  undefined,
+  applyMiddleware(saga, logger)
+);
+
+saga.run(rootSaga);
 
 export default store;
