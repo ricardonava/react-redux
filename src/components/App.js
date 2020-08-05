823be2b69d937fb69c80e60365f10707172a2dd3
Ricardo <ricardo.a.nava@gmail.com>
2020-08-04 23:09:13 -0700

    Add search functionality with redux saga

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Stories from "./Stories";
+import SearchStories from "./SearchStories";
 import "./App.css";
 
-export const App = ({ stories, onArchive }) => {
+export const App = () => {
   return (
     <div className="app">
+      <div className="interactions">
+        <SearchStories />
+      </div>
       <Stories />
     </div>
   );
