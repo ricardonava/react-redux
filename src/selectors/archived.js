02f35a79452e477382a6d246444eaf69610e2380
Ricardo <ricardo.a.nava@gmail.com>
2020-08-07 22:19:39 -0700

    Update archived.js to remove commented code

--- src/selectors/archived.js
+++ src/selectors/archived.js
@@ -1,9 +1,3 @@
-// const isNotArchived = (archivedIds) => (story) =>
-//   archivedIds.indexOf(story) === -1;
-
-// const getReadableArchived = ({ archiveState, storyState }) =>
-//   storyState.filter(isNotArchived(archiveState));
-
 const getReadableArchived = ({ archiveState }) => archiveState;
 
 export { getReadableArchived };
